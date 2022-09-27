1c04237fba1ef87416f24dfb6b93a90d0af21f2d
JicksonHD <jacques.ghajar@lau.edu>
2022-09-28 01:26:45 +0300

    added red and yellow and boolean conditions

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ function setImg(id){
     let pos = document.getElementById(id);
     console.log("I clicked on position: " + pos.innerText);
 
+    if(userIsPlaying){
+        pos.style.backgroundImage = "url(red.png)";
+        userIsPlaying=false;
+    }
+    else{
+        pos.style.backgroundImage = "url(yellow.png)";
+        userIsPlaying = true;
+    }
+
 }
