192ddaa90a91976c20d9a54ae48a3910a9a43cb2
JicksonHD <jacques.ghajar@lau.edu>
2022-09-28 01:13:48 +0300

    Added photos to grid based on position

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const cells = document.querySelectorAll('.cell');
 function setImg(id){
 
     let pos = document.getElementById(id);
-    console.log("I clicked on position: " + pos.innerHTML);
+    console.log("I clicked on position: " + pos.innerText);
 
 }
