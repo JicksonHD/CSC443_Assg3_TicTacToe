9a15667cb90758a62e98d2eb8421ca8bdf0ece77
JicksonHD <jacques.ghajar@lau.edu>
2022-09-28 23:01:35 +0300

    Added conditions for setting red or yellow circle

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+let userPlaying = 0;//0 user is playing 1 computer is playing
+let gameIsActive = true;
 
-let userIsPlaying = true;
+let game_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+const win_state  = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,4,6], [1,4,7], [2,5,8]];
 
 const cells = document.querySelectorAll('.cell');
 
@@ -8,13 +11,23 @@ function setImg(id){
     let pos = document.getElementById(id);
     console.log("I clicked on position: " + pos.innerText);
 
-    if(userIsPlaying){
-        pos.style.backgroundImage = "url(red.png)";
-        userIsPlaying=false;
+    if(game_state[pos.id]==-1 && gameIsActive){
+
+        console.log("Fete 3al if");
+
+        game_state[pos.id] = userPlaying;
+
+        if(userPlaying == 0){
+            userPlaying = 1;
+            pos.style.backgroundImage = "url(red.png)";
+        }
+        else if(userPlaying == 1){
+            userPlaying = 0;
+            pos.style.backgroundImage = "url(yellow.png)";
+        }
+       
+        
     }
-    else{
-        pos.style.backgroundImage = "url(yellow.png)";
-        userIsPlaying = true;
-    }
+    
 
 }
