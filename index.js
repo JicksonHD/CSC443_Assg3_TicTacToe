2064a7912503ace8e86ade35bf6ae2210944a928
JicksonHD <jacques.ghajar@lau.edu>
2022-09-29 21:58:41 +0300

    Added button and programmed normal tictactoe game

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let userPlaying = 0;//0 user is playing 1 computer is playing
 let gameIsActive = true;
+let counter = 0;
 
 let game_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
-const win_state  = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,4,6], [1,4,7], [2,5,8]];
+const win_states  = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]];
 
 const cells = document.querySelectorAll('.cell');
 
@@ -13,21 +14,42 @@ function setImg(id){
 
     if(game_state[pos.id]==-1 && gameIsActive){
 
-        console.log("Fete 3al if");
-
         game_state[pos.id] = userPlaying;
 
         if(userPlaying == 0){
             userPlaying = 1;
             pos.style.backgroundImage = "url(red.png)";
+            counter++;
         }
         else if(userPlaying == 1){
             userPlaying = 0;
             pos.style.backgroundImage = "url(yellow.png)";
+            counter++;
         }
-       
+        
+    }
+
+    for (let win of win_states){
+        if(game_state[win[0]] == game_state[win[1]] && game_state[win[0]] == game_state[win[2]] && game_state[win[1]] == game_state[win[2]] && game_state[win[0]] !=-1 ){
+
+            if(game_state[win[0]] == 1){
+                console.log("User has won");
+            }
+            else{
+                console.log("AI has won");
+            }
+            gameIsActive=false;
+        }
         
     }
     
+    if(counter == 9){
+        console.log("Its a tie");
+    }
 
+}
+
+function playAgain(){
+
+    
 }
