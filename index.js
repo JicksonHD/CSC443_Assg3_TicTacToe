482568449d2380639bb59f238eed29f0f85d72b6
JicksonHD <jacques.ghajar@lau.edu>
2022-10-03 01:28:55 +0300

    Fixed bugs and introduced an AI player

--- index.js
+++ index.js
@@ -19,23 +19,45 @@ function tic(id){
 
     if(game_state[pos.id]==-1 && gameIsActive){
 
-        game_state[pos.id] = userPlaying;
+        game_state[pos.id] = 0;
 
         if(userPlaying == 0){
             userPlaying = 1;
             pos.style.backgroundImage = "url(red.png)";
             counter++;
+            checkForWin();
+          //  console.log(game_state);
         }
-        else if(userPlaying == 1){
-            userPlaying = 0;
-            pos.style.backgroundImage = "url(yellow.png)";
-            counter++;
-        }
-
-        checkForWin();
+        
+            AiTurn();
+            
     }
 
 }
+
+function AiTurn(){
+
+    if(userPlaying == 1){
+
+        const empty = game_state.filter(s => s === -1);
+    
+    for(let i =0; i < game_state.length; i++){
+        if(game_state[i] === -1){
+            let pos = document.getElementById(i);
+            pos.style.backgroundImage = "url(yellow.png)";
+            userPlaying = 0;
+            game_state[i] = 1;
+            i = game_state.length;
+            counter++;
+        }
+    }
+    checkForWin();
+    }
+    
+}
+
+
+
 
 function checkForWin(){
 
@@ -43,28 +65,35 @@ function checkForWin(){
         if(game_state[win[0]] == game_state[win[1]] && game_state[win[0]] == game_state[win[2]] && game_state[win[1]] == game_state[win[2]] && game_state[win[0]] !=-1 ){
 
             if(userPlaying == 1){
+                userPlaying =0;
                 let status = document.getElementById("status");
                 status.innerText = "User has won";
                 let reset_button = document.getElementById("reset_button");
                 reset_button.style.display="block";
             }
-            else{
+            
+            else if(userPlaying == 0){
+                userPlaying = 1;
                 let status = document.getElementById("status");
                 status.innerText = "AI has won";
                 let reset_button = document.getElementById("reset_button");
                 reset_button.style.display="block";
+
             }
+
             gameIsActive=false;
+        }
+
+        if(counter == 9){
+            let status = document.getElementById("status");
+            status.innerText = "Its a tie !";
+            let reset_button = document.getElementById("reset_button");
+            reset_button.style.display="block";
         }
         
     }
 
-    if(counter == 9){
-        let status = document.getElementById("status");
-        status.innerText = "Its a tie !";
-        let reset_button = document.getElementById("reset_button");
-        reset_button.style.display="block";
-    }
+    
 
     emptyCells();
 
