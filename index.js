f91ad903cdcb7024b5ecac79bd6744d3fccbec31
JicksonHD <jacques.ghajar@lau.edu>
2022-10-03 02:13:47 +0300

    Added comments

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+//Declaring Initilizing variables
 let userPlaying = 0;//0 user is playing 1 computer is playing
 let gameIsActive = true;
 let counter = 0;
 let status = document.getElementById("status");
 
-
+//Array representing the game state where -1 is an empty spot and !-1 is a taken spot
 let game_state = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+//Array showing the indices indicating a win
 const win_states  = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]];
 
 const cells = document.querySelectorAll(".cell");
@@ -12,7 +14,7 @@ console.log(cells);
 
 
 function tic(id){
-
+//Function that sets an image red or yellow based on which player turn
 
     let pos = document.getElementById(id);
     console.log("I clicked on position: " + pos.innerText);
@@ -36,7 +38,7 @@ function tic(id){
 }
 
 function AiTurn(){
-
+//Function that tells the AI how to act when it is its turn
     if(userPlaying == 1){
 
         const empty = game_state.filter(s => s === -1);
@@ -60,6 +62,7 @@ function AiTurn(){
 
 
 function checkForWin(){
+//Function that checks if there is a winning state or a tie
 
     for (let win of win_states){
         if(game_state[win[0]] == game_state[win[1]] && game_state[win[0]] == game_state[win[2]] && game_state[win[1]] == game_state[win[2]] && game_state[win[0]] !=-1 ){
@@ -105,6 +108,7 @@ function checkForWin(){
 
 
 function playAgain(){
+//Function that resets the game once it is finished
 
     userPlaying = 0;
     counter = 0;
@@ -123,7 +127,7 @@ function playAgain(){
 }
 
 function emptyCells(){
-
+//Function that checks the empty cells
     let index_available = [];
 
     for(let i =0 ; i < game_state.length;i++){
